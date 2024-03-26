e23402b3812a1c422b539edd85e9c60e228ef611
Abhay Yadav <62054505+abhayishere@users.noreply.github.com>
2024-03-26 19:12:21 +0530

    added ellipsis

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,17 +2,24 @@
 "use client"
 
 import Link from 'next/link';
-import '../styles/navbar.css'
+import styles from '../styles/Navbar.module.css'
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="navbar">
-      <ul className="navbar_ul">
-        <li className="navbar_li"><Link href="/">Home</Link></li>
-        <li className="navbar_li"><Link href="/about">About</Link></li>
-        <li className="navbar_li"><Link href="/projects">Projects</Link></li>
-        <li className="navbar_li"><Link href="/contact">Contact</Link></li>
-        <li className="navbar_li"><Link href="/posts">Posts</Link></li>
+    <nav className={`${styles.navbar} ${isOpen ? styles.show : ''}`}>
+      <div className={`${styles.burger} ${isOpen ? styles.open : ''}`} onClick={() => setIsOpen(!isOpen)}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul className={`${styles.navbar_ul} ${isOpen ? styles.show : ''}`}>
+        <li className={styles.navbar_li}><a href="/">Home</a></li>
+        <li className={styles.navbar_li}><a href="/about">About</a></li>
+        <li className={styles.navbar_li}><a href="/projects">Projects</a></li>
+        <li className={styles.navbar_li}><a href="/contact">Contact</a></li>
+        <li className={styles.navbar_li}><a href="/posts">Posts</a></li>
       </ul>
     </nav>
   );
